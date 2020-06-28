99be0c9c7e5f617ed07b57450e32f6a9fe43dcce
Daniel <jdsosa@gmail.com>
2020-06-28 18:27:08 +0100

    Add favicon

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -20,6 +20,19 @@ AppAsset::register($this);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php $this->registerCsrfMetaTags() ?>
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="./images/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="./images/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="./images/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./images/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="./images/favicon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="./images/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="./images/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="./images/favicon/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="./images/favicon/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="./images/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="./images/favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="./images/favicon/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="./images/favicon/favicon-128.png" sizes="128x128" />
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
@@ -38,7 +51,7 @@ AppAsset::register($this);
                     ['class' => 'navbar-brand']
                 )
             ?>
-            
+
             <div class="d-inline-block">
                 <!-- Navbar toggler  -->
                 <button class="navbar-toggler hamburger hamburger-js hamburger--spring" type="button" data-toggle="collapse" data-target="#navbar_main" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
@@ -77,7 +90,7 @@ AppAsset::register($this);
                         <?= Html::a(Html::encode('Blog'), ['blog/index'], ['class' => 'nav-link']) ?>
                     </li>
                 </ul>
-                
+
             </div>
         </div>
     </nav>
