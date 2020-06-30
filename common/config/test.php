2f527fbed87c2987efee5f6b1658c85bfdbecf75
Daniel <jdsosa@gmail.com>
2020-06-30 13:55:11 +0100

    Add mysql to test

--- common/config/test.php
+++ common/config/test.php
@@ -6,9 +6,6 @@ return [
         'user' => [
             'class' => 'yii\web\User',
             'identityClass' => 'common\models\User',
-        ],
-        'db' => [
-            'dsn' => 'mysql:host=127.0.0.1;dbname=db_avila_test',
-        ],
+        ]
     ],
 ];
