bbdebd83e5fedbf282c80f5015447df8e31dc880
Daniel <jdsosa@gmail.com>
2020-07-31 12:54:40 +0100

    Adds consulting module to frontend

--- frontend/controllers/ConsultingController.php
+++ frontend/controllers/ConsultingController.php
@@ -2,16 +2,20 @@
 
 namespace frontend\controllers;
 
+use yii;
+use common\models\Consulting;
+use common\models\ConsultingSearch;
+
 class ConsultingController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
-    }
+        $consultingSearch = new ConsultingSearch;
+        $dataProvider = $consultingSearch->search(Yii::$app->request->post());
 
-    public function actionView()
-    {
-        return $this->render('view');
+        return $this->render('index', [
+          'consultingSearch' => $consultingSearch,
+          'dataProvider' => $dataProvider,
+        ]);
     }
-
 }
--- frontend/views/consulting/index.php
+++ frontend/views/consulting/index.php
@@ -5,6 +5,11 @@
 use yii\helpers\Html;
 
 $this->title = Html::encode('Asesorías');
+$i = 0;
+$textOdd = "col col-md-6 col-sm-12 col-12";
+$textEven = "col-md-6 order-lg-2";
+$imageOdd = "col col-md-6 col-sm-12 col-12 order-first order-md-last";
+$imageEven = "col-md-6 order-first order-md-first";
 ?>
 
 <section class="slice-lg slice--offset-top has-bg-cover bg-size-cover" style="background-image: url('./images/banners/banner-asesorias.jpg'); background-position: center center;">
@@ -25,110 +30,28 @@ $this->title = Html::encode('Asesorías');
     </div>
 </section>
 
-<section class="slice-lg sct-color-1">
-    <div class="container">
-        <div class="row align-items-md-center cols-xs-space cols-md-space cols-lg-space">
-            <div class="col col-md-6 col-sm-12 col-12">
-                <div class="px-3 py-3 text-center text-lg-left">
-                    <h3 class="heading heading-2 strong-500">
-                        Asesorías para adquirir propiedades nuevas
-                    </h3>
-                    <p class="lead line-height-1_8 mt-4">
-                        Recibe la información necesaria y mas completa para entender el mercado de propiedades nuevas y adquiere una propiedad nueva usando las mejores estrategias inmobiliarias.
-                    </p>
-                    <div class="btn p-0 mt-5">
-                        <a href="#" class="btn btn-styled btn-golden btn-circle text-uppercase strong-400">Presencial</a>
-                    </div>
-                    <div class="btn p-0 ml-3 mt-5">
-                        <a href="#" class="btn btn-styled btn-golden btn-circle text-uppercase strong-400">Online</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col col-md-6 col-sm-12 col-12 order-first order-md-last">
-                <img src="./images/elements/asesoria1.jpg" class="img-fluid img-center rounded z-depth-3-top">
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="slice-lg sct-color-1">
-    <div class="container">
-        <div class="row align-items-lg-center cols-xs-space cols-md-space cols-lg-space">
-            <div class="col-md-6 order-lg-2">
-                <div class="px-3 py-3 text-center text-lg-left">
-                    <h3 class="heading heading-2 strong-500 line-height-1_6">
-                        Asesorías para adquirir propiedades usadas
-                    </h3>
-                    <p class="lead line-height-1_8 mt-4">
-                        Aprende sobre el uso, desgaste natural y verdadero valor de cada propiedad en el mercado y asi poder aprovechar grandes oportunidades de inversión.
-                    </p>
-                    <div class="btn p-0 mt-5">
-                        <a href="#" class="btn btn-styled btn-golden btn-circle text-uppercase strong-400">Presencial</a>
-                    </div>
-                    <div class="btn p-0 ml-3 mt-5">
-                        <a href="#" class="btn btn-styled btn-golden btn-circle text-uppercase strong-400">Online</a>
+<?php  foreach ($dataProvider->getModels() as $consulting) : ?>
+    <section class="slice-lg sct-color-1">
+        <div class="container">
+            <div class="row align-items-md-center cols-xs-space cols-md-space cols-lg-space">
+                <div class="<?= ($i%2 === 0) ? $textEven : $textOdd ?>">
+                    <div class="px-3 py-3 text-center text-lg-left">
+                        <h3 class="heading heading-2 strong-500">
+                            <?= Html::encode($consulting->name) ?>
+                        </h3>
+                        <p class="lead line-height-1_8 mt-4">
+                            <?= Html::encode($consulting->description) ?>
+                        </p>
+                        <div class="btn p-0 ml-3 mt-5">
+                            <a href="#" class="btn btn-styled btn-golden btn-circle text-uppercase strong-400">Online</a>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-md-6 order-first order-md-first">
-                <img src="./images/elements/asesoria2.jpg" class="img-fluid img-center">
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="slice-lg sct-color-1">
-    <div class="container">
-        <div class="row align-items-md-center cols-xs-space cols-md-space cols-lg-space">
-            <div class="col col-md-6 col-sm-12 col-12">
-                <div class="px-3 py-3 text-center text-lg-left">
-                    <h3 class="heading heading-2 strong-500">
-                        Asesorías para arrendar una propiedad
-                    </h3>
-                    <p class="lead line-height-1_8 mt-4">
-                        Cada proceso de arriendo es distinto, pero se basa sobre las mismas leyes y metodología, al lograr entenderlas sabras que exigir a la hora de arrendar una propiedad, cuales son tus derechos y deberes sobre la misma.
-                    </p>
-                    <div class="btn p-0 mt-5">
-                        <a href="#" class="btn btn-styled btn-golden btn-circle text-uppercase strong-400">Presencial</a>
-                    </div>
-                    <div class="btn p-0 ml-3 mt-5">
-                        <a href="#" class="btn btn-styled btn-golden btn-circle text-uppercase strong-400">Online</a>
-                    </div>
+                <div class="<?= ($i%2 === 0) ? $imageEven : $imageOdd ?>">
+                    <?= Html::img($consulting->getImage(), ['class' => 'img-fluid img-center rounded z-depth-3-top']) ?>
                 </div>
             </div>
-
-            <div class="col col-md-6 col-sm-12 col-12 order-first order-md-last">
-                <img src="./images/elements/asesoria3.jpg" class="img-fluid img-center rounded z-depth-3-top">
-            </div>
         </div>
-    </div>
-</section>
-
-<section class="slice-lg sct-color-1">
-    <div class="container">
-        <div class="row align-items-lg-center cols-xs-space cols-md-space cols-lg-space">
-            <div class="col-md-6 order-lg-2">
-                <div class="px-3 py-3 text-center text-lg-left">
-                    <h3 class="heading heading-2 strong-500 line-height-1_6">
-                        Asesorías para evaluación de compra
-                    </h3>
-                    <p class="lead line-height-1_8 mt-4">
-                        Diseñamos un plan sistematico para que puedas evaluar si la adquisición inmobiliaria que estas por hacer o hiciste es la mas rentable para tus metas como inversionista.
-                    </p>
-                    <div class="btn p-0 mt-5">
-                        <a href="#" class="btn btn-styled btn-golden btn-circle text-uppercase strong-400">Presencial</a>
-                    </div>
-                    <div class="btn p-0 ml-3 mt-5">
-                        <a href="#" class="btn btn-styled btn-golden btn-circle text-uppercase strong-400">Online</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-6 order-first order-md-first">
-                <img src="./images/elements/asesoria4.jpg" class="img-fluid img-center">
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
+<?php $i++; endforeach; ?>
