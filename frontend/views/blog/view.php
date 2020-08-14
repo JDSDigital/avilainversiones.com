0e6bab9539081869d619c95f7b593ace9ebf80ca
Daniel <jdsosa@gmail.com>
2020-08-14 12:29:06 +0100

    Changes blog view.

--- frontend/views/blog/view.php
+++ frontend/views/blog/view.php
@@ -6,23 +6,27 @@ use yii\helpers\Html;
 
 $this->title = Html::encode('Blog');
 ?>
-<section class="slice-lg slice--offset-top has-bg-cover bg-size-cover" style="background-image: url(<?= '../images/blog/'.$model->file ?>); background-position: center center;">
+
+<section class="slice-lg slice--offset-top has-bg-cover bg-size-cover" style="background-image: url('../images/banners/banner-blog.jpg'); background-position: center center;">
     <span class="mask mask-dark--style-2"></span>
     <div class="container">
         <div class="row">
             <div class="col-lg-6">
-                <h3 class="heading heading-1 strong-400 c-white">
-                    <?= Html::encode($model->title) ?>
-                </h3>
-                <h4 class="heading heading-5 text-normal strong-300 c-white mt-4">
-                    <?= Html::encode($model->summary) ?>
-                </h4>
-
-                <ul class="inline-links inline-links--style-1 mt-4">
-                    <li>
-                        <i class="fa fa-eye"></i> <?= Html::encode($model->views) ?>
-                    </li>
-                </ul>
+                <div class="py-5">
+                    <h2 class="heading heading-inverse heading-1 strong-600 text-capitalize line-height-1_2 animated" data-animation-in="fadeInDownBig" data-animation-delay="200">
+                        <?= Html::encode($model->title) ?>
+                    </h2>
+                    <p class="lead c-white animated mt-3" data-animation-in="fadeInUpBig" data-animation-delay="200">
+                        <?= Html::encode($model->summary) ?>
+                    </p>
+                    <ul class="inline-links inline-links--style-1 mt-3">
+                        <li>
+                            <p class="heading heading-6">
+                                <i class="fa fa-eye mr-2"></i> <?= Html::encode($model->views) ?>
+                            </p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -32,7 +36,13 @@ $this->title = Html::encode('Blog');
     <div class="container container-xs">
         <div class="block block-post">
             <div class="block-body block-post-body">
+                <?= Html::img('../images/blog/'.$model->file, ['class' => 'img-fluid']) ?>
                 <?= $model->article ?>
+                <?php if ($model->source) : ?>
+                    <div class="mt-3">
+                        <?= 'Fuente: ' . Html::a($model->source, $model->source, ['target' => 'blank']) ?>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
