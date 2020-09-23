8bff4c430fe6612d0c581f5da2d802da80905067
Jorge Daniel Sosa <jdsosa@gmail.com>
2020-09-23 11:25:05 +0100

    Fixes wheel hover

--- frontend/web/js/script.js
+++ frontend/web/js/script.js
@@ -1,80 +1,86 @@
-$(document).ready(function(){
-    if (document.querySelector('#inversion') !== null) {
-        const id = '#inversion';
-        const image = $(id);
+$(document).ready(function () {
+  if (document.querySelector("#inversion") !== null) {
+    const id = "#inversion";
+    const image = $(id);
 
-        for (let index = 1; index <= 7; index++) {
-            let map = $(id.concat(index));
-            let coords = map.attr('coords').split(',');
-            let newCoords = [];
+    for (let index = 1; index <= 7; index++) {
+      let map = $(id.concat(index));
+      let coords = map.attr("coords").split(",");
+      let newCoords = [];
 
-            map.mouseover(function() {
-                $('.inversion'+index).addClass('d-block').css({
-                    left: (coords[4] * image.height()) / image.attr('height')+"px",
-                    top: (coords[5] * image.height()) / image.attr('width')+"px"
-                });
-            }).mouseout(function(){
-                $('.inversion'+index).removeClass('d-block');
+      map
+        .mouseover(function () {
+          $(".inversion" + index)
+            .addClass("d-block")
+            .css({
+              left: image.height() / 2 - 86 + "px",
+              top: (image.height() / 5) * 2 + "px"
             });
+        })
+        .mouseout(function () {
+          $(".inversion" + index).removeClass("d-block");
+        });
 
-            coords.forEach(coord => {
-                let newCoord;
+      coords.forEach(coord => {
+        let newCoord;
 
-                if (index % 2 === 0) {
-                    newCoord = (coord * image.height()) / image.attr('height');
-                } else {
-                    newCoord = (coord * image.width()) / image.attr('width');
-                }
-
-                newCoords.push(newCoord.toFixed(2));
-            });
-
-            map.attr('coords', newCoords);
+        if (index % 2 === 0) {
+          newCoord = (coord * image.height()) / image.attr("height");
+        } else {
+          newCoord = (coord * image.width()) / image.attr("width");
         }
 
-        image.maphilight();
+        newCoords.push(newCoord.toFixed(2));
+      });
+
+      map.attr("coords", newCoords);
     }
+
+    image.maphilight();
+  }
 });
 
 (function ($) {
+  $.fn.SameHeight = function () {
+    diffBoxAndContent = 0;
 
-	$.fn.SameHeight = function () {
+    this.run = function () {
+      // Boxes
+      var bhs = this.map(function () {
+        return $(this).height();
+      }).get();
+      var mbh = Math.max.apply(null, bhs);
 
-		diffBoxAndContent = 0;
+      // Contents
+      var chs = this.children()
+        .map(function () {
+          return $(this).height();
+        })
+        .get();
+      var mch = Math.max.apply(null, chs);
 
-		this.run = function () {
-			// Boxes
-			var bhs = this.map(function () {
-				return $(this).height();
-			}).get();
-			var mbh = Math.max.apply(null, bhs);
+      diffBoxAndContent = mbh - mch;
+      this.height(mbh);
+    };
 
-			// Contents
-			var chs = this.children().map(function () {
-				return $(this).height();
-			}).get();
-			var mch = Math.max.apply(null, chs);
+    this.update = function () {
+      var chs = this.children()
+        .map(function () {
+          return $(this).height();
+        })
+        .get();
+      var mch = Math.max.apply(null, chs);
+      var newHeight = mch + diffBoxAndContent;
+      this.height(newHeight);
+    };
 
-			diffBoxAndContent = mbh - mch;
-			this.height(mbh);
-		};
+    this.run();
+    my = this;
+    var resize = function () {
+      my.update();
+    };
+    $(window).resize(resize);
 
-		this.update = function () {
-			var chs = this.children().map(function () {
-				return $(this).height();
-			}).get();
-			var mch = Math.max.apply(null, chs);
-			var newHeight = mch + diffBoxAndContent;
-			this.height(newHeight);
-		};
-
-		this.run();
-		my = this;
-		var resize = function () {
-			my.update();
-		};
-		$(window).resize(resize);
-
-		return this;
-	};
+    return this;
+  };
 })(jQuery);
